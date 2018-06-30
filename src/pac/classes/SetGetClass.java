fd5274b4c84d3a38e095a1bd6c6dee39bdb8c78a
Mahmudul Hasan <40623866+MahmdulHasan@users.noreply.github.com>
2018-07-01 03:31:00 +0600

    Add files via upload

---
+++ src/pac/classes/SetGetClass.java
@@ -0,0 +1,14 @@
+package pac.classes;
+
+public class SetGetClass {
+
+    public String name2;
+    
+    public void setName(String name){
+    	    name2=name;
+    }
+    public String getName(){
+    	    return name2;
+    }
+	         
+}
